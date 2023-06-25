5f3cb419759b8a07dd1bcd43fc2879f8f74e96de
Dmaza86 <dfmaza86@gmail.com>
2023-06-25 17:38:28 +0200

    implement topbar

--- app/src/main/java/com/example/hevyclone/MainActivity.kt
+++ app/src/main/java/com/example/hevyclone/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.hevyclone
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hevyclone.screen.Workouts
 import com.example.hevyclone.ui.theme.HevyCloneTheme
@@ -20,19 +19,12 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
 @Composable
 fun WorkoutsPreview() {
     HevyCloneTheme {
-        Greeting("Android")
+        Workouts()
     }
 }
--- app/src/main/java/com/example/hevyclone/screen/Workouts.kt
+++ app/src/main/java/com/example/hevyclone/screen/Workouts.kt
@@ -3,15 +3,23 @@
 package com.example.hevyclone.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Build
+import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,22 +28,33 @@ import com.example.hevyclone.ui.theme.HevyCloneTheme
 
 @Composable
 fun Workouts() {
-    CenterAlignedTopAppBar(
-        title = { Text(text = "Workouts")},
-        navigationIcon = {
-            TextButton(onClick = {}) {
-                Text(text = "PRO", color = Color.Yellow, fontSize = 20.sp, fontWeight = FontWeight.ExtraBold)
-            }
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = "Workouts")},
+                navigationIcon = {
+                    TextButton(onClick = {}) {
+                        Text(text = "PRO",
+                            color = Color.Yellow,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.ExtraBold)
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Filled.Refresh,
+                            contentDescription = null )
+                    }
+                }
+            )
         },
-        actions = {
-            IconButton(onClick = {}) {
-                Icon(imageVector = Icons.Filled.Refresh,
-                    contentDescription = "Localized description" )
-            }
-        }
-    )
-}
+        bottomBar = {
 
+        }
+    ) { contentPadding ->
+        Text(text = "Quick Start",
+            modifier = Modifier.padding(contentPadding))    }
+}
 @Preview(
     uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
 )

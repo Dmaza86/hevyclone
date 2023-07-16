72fa66e5a556e2e570d1e3e49fa5b906df4daca0
Dmaza86 <dfmaza86@gmail.com>
2023-07-16 13:04:12 +0200

    improves table

--- app/src/main/java/com/example/hevyclone/ui/component/HevyBasicTextField.kt
+++ app/src/main/java/com/example/hevyclone/ui/component/HevyBasicTextField.kt
@@ -1,10 +1,7 @@
 package com.example.hevyclone.ui.component
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
@@ -14,17 +11,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import com.example.hevyclone.ui.theme.HevyPreviewTheme
 
 @Composable
 fun HevyBasicTextField(
+    modifier: Modifier = Modifier,
     placeholder: String,
     style: TextStyle = MaterialTheme.typography.titleLarge,
     color: Color = MaterialTheme.colorScheme.onSurface,
@@ -33,29 +30,26 @@ fun HevyBasicTextField(
     var text by remember { mutableStateOf("") }
     var isPlaceholderVisible by remember { mutableStateOf(true) }
 
-    Column() {
+    Box(modifier = modifier then Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
         if (isPlaceholderVisible) {
             Text(
                 text = placeholder,
                 style = style,
                 color = color,
-                modifier = Modifier.padding(start = 16.dp, top = 16.dp)
+                modifier = Modifier.fillMaxWidth()
             )
         }
+        BasicTextField(
+            value = text,
+            onValueChange = {
+                text = it
+                isPlaceholderVisible = it.isEmpty()
+            },
+            keyboardOptions = keyboardOptions,
+            textStyle = style,
+            modifier = Modifier.fillMaxWidth()
+        )
     }
-    BasicTextField(
-        value = text,
-        onValueChange = {
-            text = it
-            isPlaceholderVisible = it.isEmpty()
-        },
-        keyboardOptions = keyboardOptions,
-        textStyle = style,
-        modifier = Modifier
-            .heightIn(Dp.Infinity)
-            .width(IntrinsicSize.Min)
-            .padding(start = 16.dp),
-    )
 }
 
 @Preview
--- app/src/main/java/com/example/hevyclone/ui/component/HevyTable.kt
+++ app/src/main/java/com/example/hevyclone/ui/component/HevyTable.kt
@@ -1,15 +1,14 @@
 package com.example.hevyclone.ui.component
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -20,90 +19,105 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.hevyclone.ui.theme.HevyPreviewTheme
 
-
+//
+// @Composable
+// fun RowScope.HevyCell(
+//     modifier: Modifier = Modifier,
+//     content: @Composable () -> Unit
+// ) {
+//     Box(
+//         modifier = modifier then Modifier.weight(1f).fillMaxWidth()
+//     ){
+//         content()
+//     }
+// }
 @Composable
-fun HevyTable(
-    onClick: () -> Unit = {},
-    style: TextStyle = MaterialTheme.typography.titleLarge,
-    textHeader1: String,
-    textHeader2: String,
-    textHeader3: String,
-    textHeader4: String,
-    iconImageVector: ImageVector,
-    textColumn1: String,
-    textColumn2: String,
-    placeholder1: String,
-    placeholder2: String,
-) {
-    Column(
+fun HevyTable() {
+    LazyColumn(
         modifier = Modifier
-            .verticalScroll(rememberScrollState())
-            .padding(start = 16.dp, top = 16.dp)
+            .padding(16.dp)
     ) {
-        Row(
-            Modifier
-                .padding(8.dp)
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            HevyHeaderButton(text = textHeader1)
-            HevyHeaderButton(text = textHeader2)
-            HevyHeaderButton(text = textHeader3)
-            HevyHeaderButton(text = textHeader4)
-            HevyIconButton(
-                iconImageVector = iconImageVector,
-                tint = MaterialTheme.colorScheme.onSurface,
-                modifier = Modifier.size(24.dp)
-            )
-        }
-        Row(
-            Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-
-        ) {
-            TextButton(onClick) {
-                Text(
-                    text = textColumn1,
-                    color = MaterialTheme.colorScheme.onPrimary,
-                    style = style
+        item {
+            Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                HevyHeaderButton(text = "SET")
+                HevyHeaderButton(text = "PREVIOUS")
+                HevyHeaderButton(text = "KG")
+                HevyHeaderButton(text = "REPS")
+                HevyIconButton(
+                    iconImageVector = Icons.Default.Check,
+                    tint = MaterialTheme.colorScheme.onSurface,
+                    modifier = Modifier
+                        .weight(1f)
+                        .size(24.dp)
                 )
             }
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                TextButton(onClick) {
+        }
+        item {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+
+            ) {
+                TextButton(
+                    onClick = {},
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                        .padding(4.dp)
+                ) {
                     Text(
-                        text = textColumn2,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        style = style
+                        text = "1",
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        style = MaterialTheme.typography.titleLarge
                     )
                 }
-            }
-            Column {
+                TextButton(
+                    onClick = {},
+                    modifier = Modifier
+                        .weight(2f)
+                        .fillMaxWidth()
+                        .padding(4.dp)
+                ) {
+                    Text(
+                        text = "-",
+                        color = MaterialTheme.colorScheme.onSurface,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
                 HevyBasicTextField(
-                    placeholder = placeholder1,
+                    modifier = Modifier
+                        .weight(2f)
+                        .fillMaxWidth()
+                        .padding(4.dp),
+                    placeholder = "0",
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
                 )
-            }
-            Column {
                 HevyBasicTextField(
-                    placeholder = placeholder2,
+                    modifier = Modifier
+                        .weight(2f)
+                        .fillMaxWidth()
+                        .padding(4.dp),
+                    placeholder = "0",
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
                 )
-            }
-            Column(
-                modifier = Modifier.padding(end = 8.dp)
-            ) {
                 SmallFloatingActionButton(
-                    onClick,
+                    onClick = {},
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                        .padding(4.dp),
                     contentColor = MaterialTheme.colorScheme.onPrimary
                 ) {
                     Icon(
@@ -118,14 +132,19 @@ fun HevyTable(
 
 @Composable
 fun HevyHeaderButton(
+    modifier: Modifier = Modifier,
     onClick: () -> Unit = {},
     text: String,
-    color: Color = Color.Gray,
+    color: Color = MaterialTheme.colorScheme.onSurface,
     style: TextStyle = MaterialTheme.typography.titleMedium
 ) {
-    TextButton(onClick) {
+    TextButton(onClick, modifier = modifier) {
         Text(
-            text = text, color = color, style = style
+            text = text,
+            color = color,
+            style = style,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
         )
     }
 }
@@ -134,16 +153,6 @@ fun HevyHeaderButton(
 @Composable
 fun HevyTablePreview() {
     HevyPreviewTheme {
-        HevyTable(
-            textHeader1 = "SET",
-            textHeader2 = "PREVIOUS",
-            textHeader3 = "KG",
-            textHeader4 = "REPS",
-            iconImageVector = Icons.Outlined.Check,
-            textColumn1 = "1",
-            textColumn2 = "-",
-            placeholder1 = "0",
-            placeholder2 = "0"
-        )
+        HevyTable()
     }
 }
